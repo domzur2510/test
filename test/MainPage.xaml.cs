5902980127f30e975f77f6c3acbf53f3eea824bf
nencik <nenci@DESKTOP-8H7LDGL>
2017-04-06 01:37:27 +0200

    v0.002

--- test/MainPage.xaml.cs
+++ test/MainPage.xaml.cs
@@ -42,8 +42,8 @@ namespace test
                 HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(info);
                 myRequest.Method = "GET";
                 //WebResponse myResponse = myRequest.GetResponse();
-                StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
-                string result = sr.ReadToEnd();
+                //StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+               // string result = sr.ReadToEnd();
                 //sr.Close();
                 //myResponse.Close();
                 var request = WebRequest.CreateHttp(info);
